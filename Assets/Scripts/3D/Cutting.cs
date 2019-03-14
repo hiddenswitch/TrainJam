68d129c4324cbcdf8afc4ab4559727d4aa23a703
Sergio <sergiotorcanin@gmail.com>
2019-03-14 16:50:03 -0500

    The commit after the last one

---
+++ Assets/Scripts/3D/Cutting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Cutting : MonoBehaviour
+{
+    private bool cutting = false;
+
+    public GameObject knife;
+
+    Sequence sequence;
+
+    private void Start()
+    {
+        sequence = DOTween.Sequence();
+
+        var tween = knife.transform.DOLocalRotate(new Vector3(25, 180, 0), 0.1f, RotateMode.Fast);
+        sequence.Insert(0f, tween);
+        sequence.SetLoops(-1, LoopType.Yoyo);
+
+        //knife.gameObject.SetActive(false);
+
+    }
+
+    public void StartCutting()
+    {
+        if (!cutting)
+        {
+            //knife.gameObject.SetActive(true);
+            sequence.Play();
+            cutting = true;
+        }
+    }
+
+    public void StopCutting()
+    {
+        if (cutting)
+        {
+            //knife.gameObject.SetActive(false);
+            cutting = false;
+            sequence.Pause();
+        }
+    }
+}
--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -10,8 +10,11 @@ public class DragObject : MonoBehaviour
     private Vector3 lastTouchPos;
     private Vector3 dir;
     private Vector3 lastPosition;
+    private float magnitude;
     bool dragging = false;
     public float force = 2.0f;
+    Cutting lastCutting;
+    private float grabHeight = 0.2f;
 
     private void OnMouseDown()
     {
@@ -24,7 +27,6 @@ public class DragObject : MonoBehaviour
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = zCoord;
         Vector3 endPosition = Camera.main.ScreenToWorldPoint(mousePoint);
-        endPosition.y = 10f;
         return endPosition;
     }
 
@@ -34,10 +36,12 @@ public class DragObject : MonoBehaviour
         lastTouchPos = Input.mousePosition;
         //lastTouchPos = GetMouseWorldPos() + offset;
         Vector3 pos = GetMouseWorldPos() + offset;
-        pos.y = 0.4f;
+        pos.y = grabHeight;
         transform.position = pos;
 
+        transform.eulerAngles = new Vector3(0, 180, 0);
         dir = (transform.position - lastPosition).normalized;
+        magnitude = (transform.position - lastPosition).magnitude;
         lastPosition = transform.position;
     }
 
@@ -52,14 +56,35 @@ public class DragObject : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+
         if (dragging)
         {
             if (!Input.GetMouseButton(0))
             {
                 dragging = false;
                 print(dir);
-                rb.velocity = dir * force;
+                rb.velocity = dir * magnitude * force;
             }
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                Cutting cutting = hit.collider.gameObject.GetComponent<Cutting>();
+                if (cutting){
+                    lastCutting = cutting;
+                    lastCutting.StartCutting();
+                }else
+                {
+                    if (lastCutting)
+                    lastCutting.StopCutting();
+                }
+                //print(hit.collider.gameObject.name);
+            }
+        }else
+        {
+            //if (lastCutting)
+            //    lastCutting.StopCutting();
         }
     }
 }
