f21f386841e250c0e54993c68b455e3bf0d996bb
Sergio <sergiotorcanin@gmail.com>
2019-03-14 14:24:03 -0500

    The commit after

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -6,6 +6,12 @@ public class DragObject : MonoBehaviour
 {
     private Vector3 offset;
     private float zCoord;
+    public Rigidbody rb;
+    private Vector3 lastTouchPos;
+    private Vector3 dir;
+    private Vector3 lastPosition;
+    bool dragging = false;
+    public float force = 2.0f;
 
     private void OnMouseDown()
     {
@@ -18,12 +24,42 @@ public class DragObject : MonoBehaviour
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = zCoord;
         Vector3 endPosition = Camera.main.ScreenToWorldPoint(mousePoint);
-        endPosition.y = 1f;
+        endPosition.y = 10f;
         return endPosition;
     }
 
     private void OnMouseDrag()
     {
-        transform.position = GetMouseWorldPos() + offset;
+        dragging = true;
+        lastTouchPos = Input.mousePosition;
+        //lastTouchPos = GetMouseWorldPos() + offset;
+        Vector3 pos = GetMouseWorldPos() + offset;
+        pos.y = 0.4f;
+        transform.position = pos;
+
+        dir = (transform.position - lastPosition).normalized;
+        lastPosition = transform.position;
+    }
+
+    private void OnMouseExit()
+    {
+        if (dragging){
+           // dragging = false;
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (dragging)
+        {
+            if (!Input.GetMouseButton(0))
+            {
+                dragging = false;
+                print(dir);
+                rb.velocity = dir * force;
+            }
+        }
     }
 }
