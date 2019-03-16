3e60a6d1cc15fe9912f6dcecd896e0441c5f28eb
Sergio <sergiotorcanin@gmail.com>
2019-03-15 21:22:24 -0500

    More Natural Language

--- Assets/Scripts/DeliveryPlate.cs
+++ Assets/Scripts/DeliveryPlate.cs
@@ -42,7 +42,7 @@ namespace TrainJam
 
         private void UpdateIngredientDisplay()
         {
-            nextText.text = neededIngredients[ingredientIndex].ToString();
+            nextText.text = Misc.GetNameFromIngredientType(neededIngredients[ingredientIndex]);
         }
     }
 }
--- Assets/Scripts/Misc/Misc.cs
+++ Assets/Scripts/Misc/Misc.cs
@@ -11,5 +11,28 @@ namespace TrainJam
         {
             Application.targetFrameRate = 60;
         }
+
+        public static string GetNameFromIngredientType(IngredientType type)
+        {
+            switch(type)
+            {
+                case IngredientType.BlockOfBread:
+                    return "Loaf of Bread";
+                case IngredientType.BlockOfCheese:
+                    return "Brick of Cheese";
+                case IngredientType.BlockOfLettuce:
+                    return "Block of Lettuce";
+                case IngredientType.Bun:
+                    return "Slice of Bread";
+                case IngredientType.Burger:
+                    return "Burger";
+                case IngredientType.SliceOfCheese:
+                    return "Slice Of Cheese";
+                case IngredientType.SliceOfLettuce:
+                    return "Slice Of Lettuce";
+                default:
+                    return "No Name";
+            }
+        }
     }
 }
